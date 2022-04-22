96fd5c3cda61ac3123efad36c3446593695eddde
DESKTOP-1V67USP\topys <topysj0828@gmail.com>
2022-04-22 20:03:08 +0900

    Bullet, Bullet_Default

---
+++ DefaultWindow/Default/Bullet.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Obj.h"
+class CBullet :
+	public CObj
+{
+public:
+	CBullet();
+	virtual ~CBullet();
+
+public:
+	virtual void Initialize(void) override;
+	virtual int Update(void) override;
+	virtual		void	Late_Update(void);
+	virtual void Render(HDC hDC) override;
+	virtual void Release(void) override;
+
+	void Update_Rect_Missile(void);
+
+
+private:
+	POINT m_tPosin;
+	float m_fAngle2;
+	float m_fDiagonal;
+	RECT m_Misille;
+
+};
+
---
+++ DefaultWindow/Default/BulletDefault.cpp
@@ -0,0 +1,71 @@
+#include "stdafx.h"
+#include "BulletDefault.h"
+
+
+CBulletDefault::CBulletDefault()
+{
+}
+
+
+CBulletDefault::~CBulletDefault()
+{
+	Release();
+}
+
+void CBulletDefault::Initialize(void)
+{
+	m_tInfo.fCX = 10.f;
+	m_tInfo.fCY = 10.f;
+
+	m_fSpeed = 5.f;
+}
+
+int CBulletDefault::Update(void)
+{
+	if (m_bDead)
+		return OBJ_DEAD;
+
+	/*switch (m_eDir)
+	{
+	case DIR_LEFT:
+	m_tInfo.fX -= m_fSpeed;
+	break;
+	case DIR_UP:
+	m_tInfo.fY -= m_fSpeed;
+	break;
+
+	case DIR_RIGHT:
+	m_tInfo.fX += m_fSpeed;
+	break;
+
+	case DIR_DOWN:
+	m_tInfo.fY += m_fSpeed;
+	break;
+	}*/
+
+	m_tInfo.fX += m_fSpeed * cosf((m_fAngle * PI) / 180.f);
+	m_tInfo.fY -= m_fSpeed * sinf((m_fAngle * PI) / 180.f);
+
+	Update_Rect();
+
+	// Á¶°Ç ? 
+
+	return OBJ_NOEVENT;
+}
+
+void CBulletDefault::Late_Update(void)
+{
+	if (100 >= m_tRect.left || WINCX - 100 <= m_tRect.right ||
+		100 >= m_tRect.top || WINCY - 100 <= m_tRect.bottom)
+		m_bDead = true;
+}
+
+void CBulletDefault::Render(HDC hDC)
+{
+	Ellipse(hDC, m_tRect.left, m_tRect.top, m_tRect.right, m_tRect.bottom);
+}
+
+void CBulletDefault::Release(void)
+{
+
+}
