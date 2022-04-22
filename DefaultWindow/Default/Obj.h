639eec6671bba490a3fba37dab0732a196ad4306
DESKTOP-VDE3CA4\ym <pymin320@naver.com>
2022-04-22 19:28:29 +0900

    CollisionClass obj

--- DefaultWindow/Default/Obj.h
+++ DefaultWindow/Default/Obj.h
@@ -14,14 +14,21 @@ public:
 	virtual		void	Render(HDC hDC)		PURE;
 	virtual		void	Release(void)		PURE;
 
+	void		Set_Dead() { m_bDead = true; }
+	const string	Get_Side(void) const { return m_Side; }
+	const INFO&		Get_Info(void) const { return m_tInfo; }
+	const RECT&		Get_Rect(void) const { return m_tRect; }
+
 protected:
 	void		Update_Rect(void);
+	
 
 protected:
 	INFO		m_tInfo;
 	RECT		m_tRect;
+	string		m_Side;
 
 	float		m_fSpeed;
-
+	bool		m_bDead;
 };
 
